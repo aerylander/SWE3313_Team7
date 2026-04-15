44ae50b05e92b0abf4f37027cc8d2f503cafef1e
A.E. Rylander <aerylander@proton.me>
2026-04-15 17:58:41 -0400

    logic module

---
+++ src/login.java
@@ -0,0 +1,16 @@
+public class login {
+    private int[] pin;
+
+    public login(){
+        final int[] pin = {1234, 5678};
+        this.pin = pin;
+    }
+    public boolean validatePin(int inputPin){
+        for (int p : pin) {
+            if (p == inputPin) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
